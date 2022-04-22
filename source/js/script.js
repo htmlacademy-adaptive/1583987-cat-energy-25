d41acf9485e6165e9ceffc231e94225ff87da6ef
Dariia Shaihullina <dariyn@yandex.ru>
2022-04-22 16:50:25 +0500

    Исправила ошибки кроме пиксель перÑфектÐ

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,22 @@
-let mainNav = document.querySelector ('.main-nav');
+let mainHeader = document.querySelector('.main-header')
+let mainNav = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
-mainNav.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-header--nojs');
+console.log(window.innerWidth);
+console.log(document.documentElement.clientWidth);
+
+if (window.innerWidth < 768) {
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+}
 
 navToggle.addEventListener('click', (e) => {
-if (mainNav.classList.contains('main-nav--closed'))
-{mainNav.classList.remove('main-nav--closed');
-  mainNav.classList.add('main-nav--opened');}
-else {mainNav.classList.remove('main-nav--opened');
-  mainNav.classList.add('main-nav--closed');
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
   }
 })
