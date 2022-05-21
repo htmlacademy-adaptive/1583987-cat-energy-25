e2739f0664e76e9b9dd282c8560b3474ae85e340
Dariia Shaihullina <dariyn@yandex.ru>
2022-05-21 11:07:45 +0500

    ÐВнесла правки по замечаниям по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ const createWebp = () => {
 //Svg
 
 const svg = () => {
-  return gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+  return gulp.src(['source/img/**/*.svg', '!source/img/icons/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'))
 }
